5bacf04c168b11c50da3ccb18f1e7dffafad98c6
hemanth3-2004 <nhemanth2004@gmail.com>
2025-04-25 14:33:01 +0530

    Initial commit

---
+++ src/components/header/header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <header className='headerMainSection bg-gray-900 h-[20em]'>
+    <div className='container border border-2 border-solid border-red-500 rounded-full h-[18%] pt-[2em]'>
+      <div >
+        <button className='border border-2 border-solid border-green-500 rounded-full'><h2>Mane<span>Thota</span></h2></button>
+      </div>
+    </div>
+    </header>
+  )
+}
+
+export default Header;
